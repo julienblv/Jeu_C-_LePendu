1cdc4c27779ecf0d035c0a5aecd7a7bfbb5c109e
julienblv <julien.blavette31@gmail.com>
2022-09-28 16:15:51 +0200

    LettreATrouverOk

--- Mot.cs
+++ Mot.cs
@@ -5,7 +5,7 @@ class Mot
 
     
     public string motAdevinerM;
-    public char lettreSaisi;
+    public string lettreSaisi;
     //constructeur pour que le mot soit utilisable partout
     public Mot(string mot)
     {
@@ -17,11 +17,28 @@ class Mot
     {
         Console.WriteLine(motAdevinerM + " est le mot a deviner");
 
-        Console.WriteLine("Au joueur d'apres de deviner le mot : ");
+        Console.WriteLine("Au joueur d'apres de deviner le mot (choisissez une lettre) : ");
 
-        lettreSaisi = Convert.ToChar(Console.ReadLine());
+        lettreSaisi = Console.ReadLine();
 
-        Console.WriteLine("Vous avez choi la lettre" + lettreSaisi);
+        if(lettreSaisi.Length > 1)
+        {
+            Console.WriteLine("veuillez ne saisir que 1 lettre");
+            lettreSaisi = Console.ReadLine();
+        }
+        Convert.ToChar(lettreSaisi);
+
+        Console.WriteLine("Vous avez choi la lettre : " + lettreSaisi);
+
+        if(motAdevinerM.Contains(lettreSaisi))
+        {
+            Console.WriteLine(" Bravo vous avez trouvé la lettre : "+ lettreSaisi);
+
+        }
+        else
+        {
+            Console.WriteLine("Aïe Malheureusement ce n'est pas la bonne lettre");
+        }
 
 
         
