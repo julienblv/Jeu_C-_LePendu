788b9d1b78cd0888a015d93ff81de71c19a6c8f5
julienblv <julien.blavette31@gmail.com>
2022-09-28 13:49:26 +0200

    selection_Joueurs_Ok

--- Partie.cs
+++ Partie.cs
@@ -1,13 +1,37 @@
 class Partie
 {
+    public List<string>Joueur = new List<string>();
+
     public void Commencer()
     {
+        Console.WriteLine("Bienvenue à toi ! Saisies le nom du Joueur 1");
+        Joueur.Add(Console.ReadLine()); //Joueur[0]
+        Console.WriteLine("Merci ! Maintenant, saisies le nom du Joueur 2");
+        Joueur.Add(Console.ReadLine()); //Joueur[1]
+        QuiCommences();
+        faireDeviner();
+        //potence();
+        //partieGagnée();
+    }
 
+    public void QuiCommences()
+    {
+        Random rnd = new Random();
+        int chiffreAleatoiire = rnd.Next(10);
+        if (chiffreAleatoiire %2 == 0)
+        {
+           Console.WriteLine(Joueur[0]+" C'est à vous de commençer !"); 
+        }
+        else
+        {
+            Console.WriteLine(Joueur[1]+" C'est à vous de commençer !");
+        }
     }
 
     public void faireDeviner()
     {
-            //verifier si on rentres bien une lettre puis faire aller à Mot.cs
+        Console.WriteLine(" Saisissez le mot à faire deviner : ");
+        Console.ReadLine();
     }
 
     public void partieGagnée()
