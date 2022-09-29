d1cf31ef3eeae75a621f4c530f6cbc5e243bb17d
julienblv <julien.blavette31@gmail.com>
2022-09-29 15:08:12 +0200

    piti_pb_potence

--- Mot.cs
+++ Mot.cs
@@ -48,30 +48,7 @@ class Mot
             Console.WriteLine("Aïe Malheureusement ce n'est pas la bonne lettre");
             VieM = VieM-1;
         }
-
-        if(Partie.Potence.Contains(lettreSaisi))
-            {
-                Console.WriteLine("Oups vous avez déjà mis cette lettre !");
-                lettreSaisi = "";
-
-            }
-
-            // remplace les lettres dans la potence
-            for (int i = 0; i < motAdevinerM.Length; i++)
-            {
-                if (motAdevinerM.Contains(lettreSaisi[i]))
-                {
-                    
-                    string ichar = Convert.ToString(lettreSaisi[i]);
-                    Partie.Potence.Add(ichar);
-                    Console.WriteLine(string.Join(" ", Partie.Potence));
-                }
-                else
-                {
-                    Partie.Potence.Add("_");
-                    Console.WriteLine(string.Join(" ", Partie.Potence));
-                }
-            }
+        
 
 
         
